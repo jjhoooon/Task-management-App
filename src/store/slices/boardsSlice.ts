4e58085079df5d78b62f8aa082df335996ee7e58
장 재 훈 <peng2412@naver.com>
2024-06-25 20:31:30 +0900

    Board 데이터 및 타입 생성

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -1,8 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardsState = {
+    modalActive: boolean;
+    boardArray: IBoard[];
+};
+
+const initialState: TBoardsState = {
     modalActive: false,
-    boardArray: [],
+    boardArray: [
+        {
+            boardId: "board-0",
+            boardName: "첫 번째 게시물",
+            lists: [
+                {
+                    listId: "list-0",
+                    listName: "List 1",
+                    tasks: [
+                        {
+                            taskId: "task-0",
+                            taskName: "Task 1",
+                            taskDescription: "Description",
+                            taskOwner: "John",
+                        },
+                        {
+                            taskId: "task-1",
+                            taskName: "Task 2",
+                            taskDescription: "Description",
+                            taskOwner: "John",
+                        },
+                    ],
+                },
+                {
+                    listId: "list-1",
+                    listName: "List 2",
+                    tasks: [
+                        {
+                            taskId: "task-3",
+                            taskName: "Task 3",
+                            taskDescription: "Description",
+                            taskOwner: "John",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
 };
 
 const boardsSlice = createSlice({
