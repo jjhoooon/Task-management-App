f9ef58cb20e48bf8b7077bdbc8e4697558bbc95d
장 재 훈 <peng2412@naver.com>
2024-07-01 03:03:42 +0900

    SideForm Style 생성

--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -1,5 +1,10 @@
 import React, { ChangeEvent, FC, useRef, useState } from "react";
 import { FiCheck } from "react-icons/fi";
+import { icon, input, sideForm } from "./SideForm.css";
+import { useTypedDispatch } from "../../../hooks/redux";
+import { addBoard } from "../../../store/slices/boardsSlice";
+import { v4 as uuidv4 } from "uuid";
+import { addLog } from "../../../store/slices/loggerSlice";
 
 type TSideFormProps = {
     setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -8,6 +13,7 @@ type TSideFormProps = {
 
 const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
     const [inputText, setInputText] = useState("");
+    const dispatch = useTypedDispatch();
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInputText(e.target.value);
@@ -17,9 +23,33 @@ const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
         setIsFormOpen(false);
     };
 
+    const handleClick = () => {
+        if (inputText) {
+            dispatch(
+                addBoard({
+                    board: {
+                        boardId: uuidv4(),
+                        boardName: inputText,
+                        lists: [],
+                    },
+                })
+            );
+
+            dispatch(
+                addLog({
+                    logId: uuidv4(),
+                    logMessage: `게시판 등록 : ${inputText}`,
+                    logAuthor: "User",
+                    logTimestamp: String(Date.now()),
+                })
+            );
+        }
+    };
+
     return (
-        <div>
+        <div className={sideForm}>
             <input
+                className={input}
                 autoFocus
                 type="text"
                 placeholder="새로운 게시판 등록하기"
@@ -28,7 +58,7 @@ const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
                 onBlur={handleOnBlur}
                 // ref={inputRef}
             />
-            <FiCheck />
+            <FiCheck className={icon} onMouseDown={handleClick} />
         </div>
     );
 };
